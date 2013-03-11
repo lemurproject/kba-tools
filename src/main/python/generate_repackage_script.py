4c2a5b3b87cdc2605396a22cc29961902b4646f1
Shriphani Palakodety <shriphanip@gmail.com>
2013-03-11 04:59:44 -0400

    forgot to import

--- src/main/python/generate_repackage_script.py
+++ src/main/python/generate_repackage_script.py
@@ -2,6 +2,8 @@
 Generate repackage scripts
 '''
 
+import sys
+
 def produce_condor_script(prefixes):
 	for prefix in prefixes:
 		print 'condor_run \"python /bos/usr3/spalakod/kba-tools/src/main/python/kba_repackager.py /bos/tmp19/spalakod/kba-stream-corpus-2012/ %(prefix)s /bos/usr3/spalakod/kba_repack/\"' % { 'prefix':prefix }
